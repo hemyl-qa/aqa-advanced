37186c13fe8c1d692abedcbbda1ffee6cee0b798
Hemyl_qa <hemyl.qa@gmail.com>
2024-11-05 08:14:49 +0200

    HW_4.1

---
+++ jsBasics/task1.js
@@ -0,0 +1,20 @@
+//Завдання 1: Оголошення змінних для примітивних типів
+
+//Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, буль, null та undefined. 
+//Присвойте їм значення та виведіть кожну змінну на консоль.
+
+const string = 'string test';
+const number = 42;
+let boolean;
+let nullVariable;
+let und;
+
+boolean = true;
+nullVariable = null;
+
+console.log (string);
+console.log (number);
+console.log (boolean);
+console.log (nullVariable);
+console.log (und);
+
